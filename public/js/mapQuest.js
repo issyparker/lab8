c90485159e137697c97ea352507bd1747dcdd5eb
Isabelle Parker <iparker@ucsd.edu>
2021-02-26 10:28:04 +1000

    lab 8 changes

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,13 @@
 function initMap() {
-	// add your code here
+	L.mapquest.key = 'na0uOwCB3iiw0oBR2pCrhaCGQ0koQBxm';
+
+	var map = L.mapquest.map('map', {
+  		center: [32.88, -117.236],
+  		layers: L.mapquest.tileLayer('map'),
+  		zoom: 12,
+  		zoomControl: false
+	});
+
+	//add UCSD marker to map
+	L.marker([32.88, -117.236]).addTo(map);
 }
